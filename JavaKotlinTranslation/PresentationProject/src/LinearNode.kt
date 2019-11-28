9d1f787be25c6a5fe426566d8d6a4fe53927ac80
Phillip <vorcepa@gmail.com>
2019-11-27 21:50:13 -0700

    Filled out rest of Node class

--- JavaKotlinTranslation/PresentationProject/src/LinearNode.kt
+++ JavaKotlinTranslation/PresentationProject/src/LinearNode.kt
@@ -1,11 +1,27 @@
 
 
-class LinearNode<E>constructor(var element: E?, var next: LinearNode<E>?, var previous: LinearNode<E>?) {
-    public fun setNextNode(nextNode: LinearNode<E>?){
-        next = nextNode
+class LinearNode<E> constructor(var element: E?, var next: LinearNode<E>?, var previous: LinearNode<E>?) {
+    public fun setNextNode(newNextNode: LinearNode<E>?){
+        next = newNextNode
     }
 
-    public fun setPreviousNode(previousNode: LinearNode<E>?){
+    public fun getNextNode(): LinearNode<E>?{
+        return next
+    }
 
+    public fun setPreviousNode(newPreviousNode: LinearNode<E>?){
+        previous = newPreviousNode
+    }
+
+    public fun getPreviousNode(): LinearNode<E>?{
+        return previous
+    }
+
+    public fun setNodeElement(newElement: E?){
+        element = newElement
+    }
+
+    public fun getNodeElement(): E?{
+        return element
     }
 }
