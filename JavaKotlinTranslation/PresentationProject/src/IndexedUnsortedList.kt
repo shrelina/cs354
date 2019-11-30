59c3ae25d0444b02c7004c24eef04bb348d54e0b
phillipvorce <phillipvorce@u.boisestate.edu>
2019-11-29 22:00:34 -0700

    Changed interface return values to be null-unsafe, as is how we were taught to implement linked-lists

--- JavaKotlinTranslation/PresentationProject/src/IndexedUnsortedList.kt
+++ JavaKotlinTranslation/PresentationProject/src/IndexedUnsortedList.kt
@@ -4,19 +4,20 @@ interface IndexedUnsortedList<T> : Iterable<T>{
     public fun add(element: T)
     public fun addAfter(element: T, target: T)
     public fun add(index: Int, element: T)
-    public fun removeFirst(): T
-    public fun removeLast(): T
-    public fun remove(element: T): T
-    public fun remove(index: Int): T
+    public fun removeFirst(): T?
+    public fun removeLast(): T?
+    public fun remove(element: T): T?
+    public fun remove(index: Int): T?
     public fun set(index: Int, element: T)
-    public fun get(index: Int): T
-    public fun indexOf(element: T)
-    public fun first(): T
-    public fun last(): T
+    public fun get(index: Int): T?
+    public fun indexOf(element: T): Int
+    public fun first(): T?
+    public fun last(): T?
     public fun contains(target: T): Boolean
     public fun isEmpty(): Boolean
     public fun size(): Int
     public override fun toString(): String
     public override fun iterator(): Iterator<T>
-    public fun listIterator(startingIndex: Int): ListIterator<T>
+    public fun listIterator(): ListIterator<T?>
+    public fun listIterator(startingIndex: Int): ListIterator<T?>
 }
