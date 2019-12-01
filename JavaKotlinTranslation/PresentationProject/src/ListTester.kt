6267cf067b63567a94382a8139394ecdc184eecf
Phillip Vorce <vorcepa@gmail.com>
2019-11-30 21:27:25 -0700

    Filled out empty list test; ported methods that test each DLL method.  Haven't gotten to iterator testing yet.

--- JavaKotlinTranslation/PresentationProject/src/ListTester.kt
+++ JavaKotlinTranslation/PresentationProject/src/ListTester.kt
@@ -1,5 +1,6 @@
 import java.lang.Exception
 import java.lang.StringBuilder
+import java.text.DecimalFormat
 
 fun main(args: Array<String>){
     val tester = ListTester()
@@ -54,16 +55,49 @@ class ListTester{
         val LIST_B = { ELEMENT_B}
         val STRING_B = "B"
 
-        testEmptyList(newList(), "newList")
+        testEmptyList(::newList, "newList")
     }
 
-    private fun testEmptyList(scenario: IndexedUnsortedList<Int>, scenarioName: String){
+    private fun testEmptyList(scenario: () -> IndexedUnsortedList<Int?>, scenarioName: String){
+        print("\nSCENARIO: $scenarioName\n\n")
 
-        printTest(scenarioName + "_testRemoveFirst", testRemoveFirst(scenario, null, Result.NoSuchElement))
+        try {
+            printTest(scenarioName + "_testRemoveFirst", testRemoveFirst(scenario, null, Result.NoSuchElement))
+            printTest(scenarioName + "_testRemoveLast", testRemoveLast(scenario, null, Result.NoSuchElement))
+            printTest(scenarioName + "_testRemoveX", testRemoveElement(scenario, null, Result.NoSuchElement))
+            printTest(scenarioName + "_testFirst", testFirst(scenario, null, Result.NoSuchElement))
+            printTest(scenarioName + "_testLast", testLast(scenario, null, Result.NoSuchElement))
+            printTest(scenarioName + "_testContainsX", testContains(scenario, ELEMENT_X, Result.False))
+            printTest(scenarioName + "_testIsEmpty", testIsEmpty(scenario, Result.True))
+            printTest(scenarioName + "_testSize", testSize(scenario, 0))
+            printTest(scenarioName + "_testToString", testToString(scenario, Result.ValidString))
+            printTest(scenarioName + "_testAddToFront", testAddToFront(scenario, ELEMENT_X, Result.NoException))
+            printTest(scenarioName + "_testAddToRear", testAddToRear(scenario, ELEMENT_X, Result.NoException))
+            printTest(scenarioName + "_testAddAfterX", testAddAfter(scenario, ELEMENT_X, ELEMENT_Z, Result.NoSuchElement))
+            printTest(scenarioName + "_testAddAtIndexNeg1", testAddAtIndex(scenario, -1, ELEMENT_X, Result.IndexOutOfBounds))
+            printTest(scenarioName + "_testAddAtIndex0", testAddAtIndex(scenario, 0, ELEMENT_X, Result.NoException));
+            printTest(scenarioName + "_testAddAtIndex1", testAddAtIndex(scenario, 1, ELEMENT_X, Result.IndexOutOfBounds));
+            printTest(scenarioName + "_testSetNeg1", testSet(scenario, -1, ELEMENT_X, Result.IndexOutOfBounds));
+            printTest(scenarioName + "_testSet0", testSet(scenario, 0, ELEMENT_X, Result.IndexOutOfBounds));
+            printTest(scenarioName + "_testAddX", testAdd(scenario, ELEMENT_X, Result.NoException));
+            printTest(scenarioName + "_testGetNeg1", testGet(scenario, -1, null, Result.IndexOutOfBounds));
+            printTest(scenarioName + "_testGet0", testGet(scenario, 0, null, Result.IndexOutOfBounds));
+            printTest(scenarioName + "_testIndexOfX", testIndexOf(scenario, ELEMENT_X, -1));
+            printTest(scenarioName + "_testRemoveAtNeg1", testRemoveAt(scenario, -1, null, Result.IndexOutOfBounds));
+            printTest(scenarioName + "_testRemoveAt0", testRemoveAt(scenario, 0, null, Result.IndexOutOfBounds));
+        }
+        catch(e: Exception){
+            print("***UNABLE TO RUN/COMPLETE %s*** \n $scenarioName TESTS\n")
+        }
+        finally{
+            if (printSectionSummaries){
+                printSectionSummary()
+            }
+        }
     }
 
-    private fun newList(): IndexedUnsortedList<Int>{
-        return IUDoubleLinkedList<Int>(null, null, 0)
+    private fun newList(): IndexedUnsortedList<Int?>{
+        return IUDoubleLinkedList<Int?>(null, null, 0)
     }
 
     private fun printTest(testDesc: String, result: Boolean){
@@ -90,9 +124,23 @@ class ListTester{
         }
     }
 
-    private fun testRemoveFirst(list: IndexedUnsortedList<Int>, expectedElement: Int?, expectedResult: Result) : Boolean{
+    private fun printSectionSummary(){
+        secTotal = totalRun - secTotal
+        secPasses = passes - secPasses
+        secFails = failures - secFails
+        print("Section Tests: $secTotal, Passed: $secPasses, Failed: $secFails\n")
+        secPasses = passes
+        secFails = failures
+        val percentPassed = passes * 100.0 / totalRun
+        val decimal = DecimalFormat("#.00")
+        val percentPassedStr = decimal.format(percentPassed)
+        print("Tests run so far: $totalRun, Passed: $passes ($percentPassedStr%), Failed: $failures\n")
+    }
+
+    private fun testRemoveFirst(listToBuild: () -> IndexedUnsortedList<Int?>, expectedElement: Int?, expectedResult: Result) : Boolean{
         var result: ListTester.Result
         try{
+            var list = listToBuild()
             val retVal = list.removeFirst()
             if (retVal!!.equals(expectedElement)){
                 result = Result.MatchingValue
@@ -117,5 +165,445 @@ class ListTester{
 
         return result == expectedResult
     }
+
+    private fun testRemoveLast(listToBuild: () -> IndexedUnsortedList<Int?>, expectedElement: Int?, expectedResult: Result) : Boolean{
+        var result: ListTester.Result
+        try{
+            var list = listToBuild()
+            val retVal = list.removeLast()
+            if (retVal!!.equals(expectedElement)){
+                result = Result.MatchingValue
+            }
+            else{
+                result = Result.Fail
+            }
+        }
+        catch(e: NoSuchElementException){
+            result = Result.NoSuchElement
+        }
+        catch (e: Exception){
+            val sb = StringBuilder()
+            sb.append(e.toString())
+            sb.append(" caught unexpected ")
+            sb.append(e.printStackTrace())
+            sb.append("\n")
+            sb.append("testRemoveLast")
+
+            result = Result.UnexpectedException
+        }
+
+        return result == expectedResult
+    }
+
+    private fun testRemoveElement(listToBuild: () -> IndexedUnsortedList<Int?>, element: Int?, expectedResult: Result) : Boolean{
+        var result: ListTester.Result
+        try{
+            var list = listToBuild()
+            val retVal = list.remove(element)
+            if (retVal!!.equals(element)){
+                result = Result.MatchingValue
+            }
+            else{
+                result = Result.Fail
+            }
+        }
+        catch(e: NoSuchElementException){
+            result = Result.NoSuchElement
+        }
+        catch (e: Exception){
+            val sb = StringBuilder()
+            sb.append(e.toString())
+            sb.append(" caught unexpected ")
+            sb.append(e.printStackTrace())
+            sb.append("\n")
+            sb.append("testRemoveElement")
+
+            result = Result.UnexpectedException
+        }
+
+        return result == expectedResult
+    }
+
+    private fun testFirst(listToBuild: () -> IndexedUnsortedList<Int?>, expectedElement: Int?, expectedResult: Result) : Boolean{
+        var result: ListTester.Result
+        try{
+            var list = listToBuild()
+            val retVal = list.first()
+            if (retVal!!.equals(expectedElement)){
+                result = Result.MatchingValue
+            }
+            else{
+                result = Result.Fail
+            }
+        }
+        catch(e: NoSuchElementException){
+            result = Result.NoSuchElement
+        }
+        catch (e: Exception){
+            val sb = StringBuilder()
+            sb.append(e.toString())
+            sb.append(" caught unexpected ")
+            sb.append(e.printStackTrace())
+            sb.append("\n")
+            sb.append("testFirst")
+
+            result = Result.UnexpectedException
+        }
+
+        return result == expectedResult
+    }
+
+    private fun testLast(listToBuild: () -> IndexedUnsortedList<Int?>, expectedElement: Int?, expectedResult: Result) : Boolean{
+        var result: ListTester.Result
+        try{
+            var list = listToBuild()
+            val retVal = list.last()
+            if (retVal!!.equals(expectedElement)){
+                result = Result.MatchingValue
+            }
+            else{
+                result = Result.Fail
+            }
+        }
+        catch(e: NoSuchElementException){
+            result = Result.NoSuchElement
+        }
+        catch (e: Exception){
+            val sb = StringBuilder()
+            sb.append(e.toString())
+            sb.append(" caught unexpected ")
+            sb.append(e.printStackTrace())
+            sb.append("\n")
+            sb.append("testLast")
+
+            result = Result.UnexpectedException
+        }
+
+        return result == expectedResult
+    }
+
+    private fun testContains(listToBuild: () -> IndexedUnsortedList<Int?>, element: Int?, expectedResult: Result) : Boolean{
+        var result: ListTester.Result
+        try{
+            var list = listToBuild()
+            if (list.contains(element)){
+                result = Result.True
+            }
+            else{
+                result = Result.False
+            }
+        }
+        catch (e: Exception){
+            val sb = StringBuilder()
+            sb.append(e.toString())
+            sb.append(" caught unexpected ")
+            sb.append(e.printStackTrace())
+            sb.append("\n")
+            sb.append("testContains")
+
+            result = Result.UnexpectedException
+        }
+
+        return result == expectedResult
+    }
+
+    private fun testIsEmpty(listToBuild: () -> IndexedUnsortedList<Int?>, expectedResult: Result) : Boolean{
+        var result: ListTester.Result
+        try{
+            var list = listToBuild()
+            if (list.isEmpty()){
+                result = Result.True
+            }
+            else{
+                result = Result.False
+            }
+        }
+        catch (e: Exception){
+            val sb = StringBuilder()
+            sb.append(e.toString())
+            sb.append(" caught unexpected ")
+            sb.append(e.printStackTrace())
+            sb.append("\n")
+            sb.append("testIsEmpty")
+
+            result = Result.UnexpectedException
+        }
+
+        return result == expectedResult
+    }
+
+    private fun testSize(listToBuild: () -> IndexedUnsortedList<Int?>, expectedSize: Int) : Boolean{
+        try{
+            var list = listToBuild()
+            return (list.size() == expectedSize)
+        }
+        catch (e: Exception){
+            val sb = StringBuilder()
+            sb.append(e.toString())
+            sb.append(" caught unexpected ")
+            sb.append(e.printStackTrace())
+            sb.append("\n")
+            sb.append("testIsEmpty")
+
+            return false
+        }
+    }
+
+    private fun testToString(listToBuild: () -> IndexedUnsortedList<Int?>, expectedResult: Result) : Boolean{
+        var result: Result
+        try{
+            var list = listToBuild()
+            var str = list.toString().trim()
+            if (showToString){
+                print("toString() output: $str\n")
+            }
+            if (str.length < (list.size() + list.size() / 2 + 2)){
+                result = Result.Fail
+            }
+            else{
+                val lastChar = str.last()
+                val firstChar = str.first()
+                if (firstChar != '[' || lastChar != ']'){
+                    result = Result.Fail
+                }
+                else if (str.contains("@")
+                        && !str.contains(" ")
+                        && Character.isLetter(str.first())
+                        && (Character.isDigit(str.last()) || (lastChar >= 'a' && lastChar <= 'f'))){
+                    result = Result.Fail
+                }
+                else{
+                    result = Result.ValidString
+                }
+            }
+        }
+        catch (e: Exception){
+            val sb = StringBuilder()
+            sb.append(e.toString())
+            sb.append(" caught unexpected ")
+            sb.append(e.printStackTrace())
+            sb.append("\n")
+            sb.append("testIsEmpty")
+
+            result = Result.UnexpectedException
+        }
+
+        return result == expectedResult
+    }
+
+    private fun testAddToFront(listToBuild: () -> IndexedUnsortedList<Int?>, element: Int?, expectedResult: Result) : Boolean{
+        var result: ListTester.Result
+        try{
+            var list = listToBuild()
+            list.addToFront(element)
+            result = Result.NoException
+        }
+        catch (e: Exception){
+            val sb = StringBuilder()
+            sb.append(e.toString())
+            sb.append(" caught unexpected ")
+            sb.append(e.printStackTrace())
+            sb.append("\n")
+            sb.append("testAddToFront")
+
+            result = Result.UnexpectedException
+        }
+
+        return result == expectedResult
+    }
+
+    private fun testAddToRear(listToBuild: () -> IndexedUnsortedList<Int?>, element: Int?, expectedResult: Result) : Boolean{
+        var result: ListTester.Result
+        try{
+            var list = listToBuild()
+            list.addToRear(element)
+            result = Result.NoException
+        }
+        catch (e: Exception){
+            val sb = StringBuilder()
+            sb.append(e.toString())
+            sb.append(" caught unexpected ")
+            sb.append(e.printStackTrace())
+            sb.append("\n")
+            sb.append("testAddToRear")
+
+            result = Result.UnexpectedException
+        }
+
+        return result == expectedResult
+    }
+
+    private fun testAddAfter(listToBuild: () -> IndexedUnsortedList<Int?>, target: Int?, element: Int?, expectedResult: Result) : Boolean{
+        var result: ListTester.Result
+        try{
+            var list = listToBuild()
+            list.addAfter(element, target)
+            result = Result.NoException
+        }
+        catch(e: NoSuchElementException){
+            result = Result.NoSuchElement
+        }
+        catch (e: Exception){
+            val sb = StringBuilder()
+            sb.append(e.toString())
+            sb.append(" caught unexpected ")
+            sb.append(e.printStackTrace())
+            sb.append("\n")
+            sb.append("testAddAfter")
+
+            result = Result.UnexpectedException
+        }
+
+        return result == expectedResult
+    }
+
+    private fun testAddAtIndex(listToBuild: () -> IndexedUnsortedList<Int?>, index: Int, element: Int?, expectedResult: Result) : Boolean{
+        var result: ListTester.Result
+        try{
+            var list = listToBuild()
+            list.add(index, element)
+            result = Result.NoException
+        }
+        catch(e: IndexOutOfBoundsException){
+            result = Result.IndexOutOfBounds
+        }
+        catch (e: Exception){
+            val sb = StringBuilder()
+            sb.append(e.toString())
+            sb.append(" caught unexpected ")
+            sb.append(e.printStackTrace())
+            sb.append("\n")
+            sb.append("testAddAtIndex")
+
+            result = Result.UnexpectedException
+        }
+
+        return result == expectedResult
+    }
+
+    private fun testSet(listToBuild: () -> IndexedUnsortedList<Int?>, index: Int, element: Int?, expectedResult: Result) : Boolean{
+        var result: ListTester.Result
+        try{
+            var list = listToBuild()
+            list.set(index, element)
+            result = Result.NoException
+        }
+        catch(e: IndexOutOfBoundsException){
+            result = Result.IndexOutOfBounds
+        }
+        catch (e: Exception){
+            val sb = StringBuilder()
+            sb.append(e.toString())
+            sb.append(" caught unexpected ")
+            sb.append(e.printStackTrace())
+            sb.append("\n")
+            sb.append("testSet")
+
+            result = Result.UnexpectedException
+        }
+
+        return result == expectedResult
+    }
+
+    private fun testAdd(listToBuild: () -> IndexedUnsortedList<Int?>, element: Int?, expectedResult: Result) : Boolean{
+        var result: ListTester.Result
+        try{
+            var list = listToBuild()
+            list.add(element)
+            result = Result.NoException
+        }
+        catch(e: IndexOutOfBoundsException){
+            result = Result.IndexOutOfBounds
+        }
+        catch (e: Exception){
+            val sb = StringBuilder()
+            sb.append(e.toString())
+            sb.append(" caught unexpected ")
+            sb.append(e.printStackTrace())
+            sb.append("\n")
+            sb.append("testSet")
+
+            result = Result.UnexpectedException
+        }
+
+        return result == expectedResult
+    }
+
+    private fun testGet(listToBuild: () -> IndexedUnsortedList<Int?>, index: Int, expectedElement: Int?, expectedResult: Result) : Boolean{
+        var result: ListTester.Result
+        try{
+            var list = listToBuild()
+            val retVal = list.get(index)
+            if (retVal!!.equals(expectedElement)){
+                result = Result.MatchingValue
+            }
+            else{
+                result = Result.Fail
+            }
+
+        }
+        catch(e: IndexOutOfBoundsException){
+            result = Result.IndexOutOfBounds
+        }
+        catch (e: Exception){
+            val sb = StringBuilder()
+            sb.append(e.toString())
+            sb.append(" caught unexpected ")
+            sb.append(e.printStackTrace())
+            sb.append("\n")
+            sb.append("testSet")
+
+            result = Result.UnexpectedException
+        }
+
+        return result == expectedResult
+    }
+
+    private fun testIndexOf(listToBuild: () -> IndexedUnsortedList<Int?>, element: Int?, expectedIndex: Int) : Boolean{
+        try{
+            var list = listToBuild()
+            return list.indexOf(element) == expectedIndex
+        }
+        catch (e: Exception){
+            val sb = StringBuilder()
+            sb.append(e.toString())
+            sb.append(" caught unexpected ")
+            sb.append(e.printStackTrace())
+            sb.append("\n")
+            sb.append("testSet")
+
+            return false
+        }
+    }
+
+    private fun testRemoveAt(listToBuild: () -> IndexedUnsortedList<Int?>, index: Int, expectedElement: Int?, expectedResult: Result) : Boolean{
+        var result: Result
+        try{
+            var list = listToBuild()
+            val retVal = list.removeAt(index)
+            if (retVal!!.equals(expectedElement)){
+                result = Result.MatchingValue
+            }
+            else{
+                result = Result.Fail
+            }
+        }
+        catch(e: IndexOutOfBoundsException){
+            result = Result.IndexOutOfBounds
+        }
+        catch (e: Exception){
+            val sb = StringBuilder()
+            sb.append(e.toString())
+            sb.append(" caught unexpected ")
+            sb.append(e.printStackTrace())
+            sb.append("\n")
+            sb.append("testSet")
+
+            result = Result.UnexpectedException
+        }
+
+        return result == expectedResult
+    }
+
 
 }
